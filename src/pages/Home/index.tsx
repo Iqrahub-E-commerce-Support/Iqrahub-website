fba4f0313176895995ae5af234a15d2e5c93325e
abrar aboobacker <abraraboobackerkk@gmail.com>
2025-01-24 17:39:50 +0530

    feat - added areaofExpertise component in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,13 +3,13 @@ import Hero from "./components/Hero";
 import { MdArrowForward } from "react-icons/md";
 import { MdPlayArrow } from "react-icons/md";
 import GradientButton from "@/components/GradientButton";
-import Marquee from "@/components/Marquee";
+import AreaOfExpertise from "./components/AreaOfExpertise";
 const Home = () => {
   return (
     <>
       <section
         // className="h-[450px] bg-cover bg-center bg-gradient-to-r from-[#FFC506] to-[#C5261F] "
-        className="h-[550px] bg-cover bg-center flex flex-col justify-center items-center gap-9  xs:pt-[15.2rem] sm:pt-[8%]"
+        className="h-[550px] bg-cover  flex flex-col justify-center items-center gap-9  xs:pt-[15.2rem] sm:pt-[5%]"
         style={{ backgroundImage: `url(${bg}) ` }}
       >
         <Hero />
@@ -21,35 +21,10 @@ const Home = () => {
             <MdPlayArrow size={30} />
           </GradientButton>
         </div>
-
       </section>
-        <Marquee >
-        {/* <span className="text-4xl mx-4">Marquee Item 1</span>
-    <span className="text-4xl mx-4">Marquee Item 2</span>
-    <span className="text-4xl mx-4">Marquee Item 3</span>
-    <span className="text-4xl mx-4">Marquee Item 4</span>*/}
-    {/* <span className="text-4xl mx-4"></span>  */}
-    <div className="flex gap-10">
-    <div className="h-40 w-40 bg-yellow">
-    sdf
-   </div>
-   <div className="h-40 w-40 bg-yellow">
-    sdf
-   </div>
-   <div className="h-40 w-40 bg-yellow">
-    sdf
-   </div>
-   <div className="h-40 w-40 bg-yellow">
-    sdf
-   </div>
-   <div className="h-40 w-40 bg-yellow">
-    sdf
-   </div>
-  
-  
-    </div>
-   
-        </Marquee>
+      <section>
+        <AreaOfExpertise/>
+      </section>
     </>
   );
 };
