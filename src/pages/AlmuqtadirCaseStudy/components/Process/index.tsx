bddef241a45a2a715ee0c916c570897f381b00b1
abrar aboobacker <abraraboobackerkk@gmail.com>
2025-02-20 15:10:19 +0530

    changes

--- src/pages/AlmuqtadirCaseStudy/components/Process/index.tsx
+++ src/pages/AlmuqtadirCaseStudy/components/Process/index.tsx
@@ -6,34 +6,39 @@ import PrecessCal from "@/assets/images/almuqtadir/ProcessBg.png";
 const Process = () => {
   return (
     <>
-      <div className="relative custom-container border-x-2 border-gold-900 ">
+      <div className="relative custom-container border-x-2 border-gold-900 "
+      style={{
+        boxShadow:
+          "4px 0px 0px rgba(193,149,53,0.4), -4px 0px 0px rgba(193,149,53,0.4)",
+      }}
+      >
         <img
           src={PrecessCal}
           alt="Background"
-          className="absolute top-0 left-[74%] transform -translate-x-1/2 w-full h-full object-cover opacity-60 z-0"
+          className="absolute top-0 xs:left-[50%] md:left-[74%] transform -translate-x-1/2 w-full xs:h-[110%] sm:h-full object-cover opacity-60 z-0"
         />
         <img
           src={leftDesign}
           alt="leftDesign"
-          className="relatvie h-[350px] pt-16"
+          className="relatvie xs:h-[450px] sm:h-[350px] xs:pt-60 sm:pt-16"
         />
-        <div className="-mt-[330px]">
-          <div className="flex flex-col items-center">
+        <div className="xs:-mt-[380px] sm:-mt-[330px]">
+          <div className="flex flex-col sm:items-center xs:px-3">
             <CustomizableHeading
               firstTxt="THE PROCESS"
               secondTxt="OF MUQTADIR"
-              MainStyle="text-gold font-extrabold md:text-[48px] "
+              MainStyle="text-gold font-black md:text-[48px] xs:text-center "
               SpecialCharacterStyle="font-thin text-white"
             />
-            <p className=" mt-2 w-[720px] text-center text-[18px]">
+            <p className=" mt-2 sm:w-[720px] text-center text-[18px]">
               Recognizing that their main customers were wedding buyers
               purchasing over 40 grams of gold in advance, we analyzed their
               sales process and identified key challenges.
             </p>
           </div>
-          <div className="mt-20 grid grid-cols-[2fr_1.5fr] gap-4 px-10">
+          <div className="mt-20 grid xs:grid-cols-1 md:grid-cols-[2fr_1.5fr] gap-4  px-10">
             <div className="grid grid-row-3 gap-4">
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid sm:grid-cols-2 gap-4">
                 <div className="glassy-bg flex flex-col justify-center border-2 border-gold rounded-[20px] px-8 ">
                   <h6 className="font-black text-gold text-[20px] capitalize ">
                     Complex decision-making: 
@@ -53,7 +58,7 @@ const Process = () => {
                   </p>
                 </div>
               </div>
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid sm:grid-cols-2 gap-4">
                 <div className="glassy-bg flex flex-col justify-center border-2 border-gold rounded-[20px] px-8 ">
                   <h6 className="font-black text-gold text-[20px] capitalize">
                     No digital engagement:
@@ -84,13 +89,13 @@ const Process = () => {
               </div>
             </div>
 
-            <img src={process} alt="process" className=" -mt-16 z-10 " />
+            <img src={process} alt="process" className="xs:-mt-8 sm:-mt-16 md:mt-[30px] lg:-mt-16 z-10 xs:h-[400px] sm:h-full md:h-[90%] lg:h-full  xs:object-cover sm:object-cover  " />
           </div>
         </div>
         <img
           src={rightDesign}
           alt="rightDesign"
-          className="h-[350px] place-self-end -mt-80"
+          className=" xs:h-[450px] sm:h-[350px] place-self-end -mt-80"
         />
       </div>
     </>
